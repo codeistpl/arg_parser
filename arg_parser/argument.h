ed657967f9ea891d4afbad14c236887317925217
Michal Sopniewski <codeist@codeist.pl>
2021-03-27 16:56:57 +0100

    argument factories added

--- arg_parser/argument.h
+++ arg_parser/argument.h
@@ -16,6 +16,21 @@ inline bool argument_match(const Argument &arg, std::string_view match) {
     return arg.name == match || arg.short_name == match;
 }
 
+inline Argument make_arg(const std::string &name,
+                         const std::string &short_name = "",
+                         const std::string &description = "",
+                         const std::string &default_value = "",
+                         const std::vector<std::string> &allowed_values = {}) {
+    Argument arg{};
+    arg.name = name;
+    arg.short_name = short_name;
+    arg.description = description;
+    arg.default_value = default_value;
+    arg.allowed_values = allowed_values;
+
+    return arg;
+}
+
 inline Argument make_flag(const std::string &name,
                           const std::string &short_name = "",
                           const std::string &description = "") {
--- test_app/main.cpp
+++ test_app/main.cpp
@@ -7,7 +7,12 @@ int main(int argc, char const *argv[]) {
     ArgParser parser(
         "This is just an example program presenting argument parser working.");
     parser.add_arg(make_flag("--help", "-h", "Prints help page."));
+    parser.add_arg(make_flag("--verbose", "-V", "Enables verbose output!"));
+    parser.add_arg(
+        make_arg("--level", "-l", "Level", "low", {"low", "medium", "high"}));
     auto args = parser.parse_args(argc, argv);
+
+    std::cout << "args and values:" << std::endl;
 
     for (const auto &pair : args) {
         std::cout << pair.first << "\t" << pair.second << std::endl;
