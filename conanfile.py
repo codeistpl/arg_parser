16b8d4a312b7b048783d5f832aadd3720f3db77c
Michal Sopniewski <codeist@codeist.pl>
2021-03-27 21:33:45 +0100

    conan packaging added

--- conanfile.py
+++ conanfile.py
@@ -1,13 +1,21 @@
 from conans import ConanFile, CMake
 
 
-class BraceChecker(ConanFile):
+class ArgumentParser(ConanFile):
+    name = "arg_parser"
+    version = "0.0.0"
+    license = "MIT"
+    url = "<Package recipe repository url here, for issues about the package>"
+    description = "Simple argument parser for CLI in C++"
     settings = "os", "compiler", "build_type", "arch"
     requires = [
-        "gtest/1.8.1@bincrafters/stable",  # package 1.10.0 have some issue with conanfile ... find package dosn't work with build_type=Debug
+        "gtest/1.8.1@bincrafters/stable",  # package 1.10.0 have some issue with conanfile ... find package doesn't work with build_type=Debug
         "fmt/7.1.3"  # std::format implementation
     ]
+    options = {"shared": [True, False]}
+    default_options = {"shared": False}
     generators = "cmake", "cmake_paths"
+    exports_sources = "*"
 
     def imports(self):
         self.copy("*.dll", dst="bin", src="bin")  # From bin to bin
@@ -18,3 +26,10 @@ class BraceChecker(ConanFile):
         cmake.definitions["CMAKE_EXPORT_COMPILE_COMMANDS"] = "ON"
         cmake.configure()
         cmake.build()
+
+    def package(self):
+        cmake = CMake(self)
+        cmake.install()
+
+    def package_info(self):
+        self.cpp_info.libs = ["arg_parser"]
